b95551f1a83fa3854f677dab24b77d15aa5c05ae
wolforcept <miguelfeotorres@gmail.com>
2022-07-21 10:22:21 +0100

    updated utils

---
+++ src/main/java/wolforce/utils/registry/UtilItemRegistry.java
@@ -0,0 +1,57 @@
+package wolforce.utils.registry;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class UtilItemRegistry {
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.FIELD)
+	public @interface RegItem {
+		public String regName() default "";
+	}
+
+	private static Class<?> itemsClass;
+
+	public static void init(Class<?> itemsClass) {
+		UtilItemRegistry.itemsClass = itemsClass;
+	}
+
+	@SubscribeEvent
+	public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
+		try {
+			IForgeRegistry<Item> registry = event.getRegistry();
+
+			for (Field field : itemsClass.getDeclaredFields()) {
+
+				if (field.isAnnotationPresent(RegItem.class) && field.get(null) instanceof Item item) {
+					RegItem reg = field.getAnnotation(RegItem.class);
+					item.setRegistryName(reg.regName());
+					registry.register(item);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+//	@SubscribeEvent
+//	public static void registerItems(final RegistryEvent.Register<Item> event) {
+//		HearthWell.setupItems();
+//		IForgeRegistry<Item> registry = event.getRegistry();
+//		for (Item item : HearthWell.items.values()) {
+//			registry.register(item);
+//		}
+//	}
+
+}
